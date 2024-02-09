185c69de1d5eab624f0b6770ab3d5527a876fa6a
DanPark13 <dpark0703@gmail.com>
2024-02-09 10:21:00 -0500

    Add TodoList-like UI

--- App.js
+++ App.js
@@ -1,74 +1,110 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import colors from "./Colors";
-import tempData from "./tempData";
-import TodoList from "./components/TodoList";
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import Task from './components/Task';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{flexDirection: "row"}}>
-          <View style={styles.divider} />
-          <Text style={styles.title}>
-            Repito <Text style={{fontWeight: "300", color: colors.blue }}>Lists</Text>
-          </Text>
-          <View style={styles.divider} />
-        </View>
+export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
 
-        <View style={{ marginVertical: 48 }}>
-          <TouchableOpacity style={styles.addList}>
-            <AntDesign name="plus" size={16} color={colors.blue} />
-          </TouchableOpacity>
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
 
-          <Text style={styles.add}>Add List</Text>
-        </View>
-        <View style={{height: 275, paddingLeft: 32}}>
-          <FlatList 
-            data={tempData} 
-            keyExtractor={item => item.name} 
-            horizontal={true} 
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => <TodoList list={item} /> }
-          />
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
+                  <Task text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
       </View>
-    );
-  }
+        
+      </ScrollView>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   },
-  divider: {
-    backgroundColor: colors.lightBlue,
-    height: 1,
-    flex: 1,
-    alignSelf: "center"
-  },
-  title: {
-    fontSize: 38,
-    fontWeight: "800",
-    color: colors.black,
-    paddingHorizontal: 64
-  },
-  addList: {
-    borderWidth: 2,
-    borderColor: colors.lightBlue,
-    borderRadius: 4,
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  add: {
-    color: colors.blue,
-    fontWeight: "600",
-    fontSize: 14,
-    marginTop: 8
-  }
+  addText: {},
 });
