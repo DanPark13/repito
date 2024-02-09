4a774b5102d04cf5c66f9d1e820fd2b94dd167dc
DanPark13 <dpark0703@gmail.com>
2024-02-08 21:59:35 -0500

    Base Project Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>A List Style App That Sends a Bullet Point to you once a day for spaced repetition</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,5 @@
+# Repito
+
+Have you ever struggled with remembering the small random bits of knowledge you gained in your life? 
+
+Repito is an app that allows you to jot down memorable knowledge bits and every day, Repito will send you a notification of the knowlege bit you were missing in your day.
