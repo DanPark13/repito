fd72a5a4035e30f507829eb568a59cddd56f82a9
DanPark13 <dpark0703@gmail.com>
2024-02-09 09:00:11 -0500

    Basic UI

---
+++ tempData.js
@@ -0,0 +1,27 @@
+// Handles List Data
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            }
+        ]
+    }
+]
